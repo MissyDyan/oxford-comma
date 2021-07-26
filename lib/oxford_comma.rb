029919c200e9cbbbabaef0e708992d7fc124eb98
Diane Nguyen <diane.nguyen@gmail.com>
2021-07-25 23:35:57 -0700

    Oxford Comma List Formatter assignment completed.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
 
+    case array.length
+    when 1 
+         "kiwi"
+    when 2
+         array.insert(1, "and")
+         array.join(' ')
+    when 3
+        array[-1] = "and #{array[-1]}"
+        array.join(', ')
+    else 
+        array[-1] = "and #{array[-1]}"
+        array.join(', ')
+    end
+
 end
